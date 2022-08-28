334053caf521027494d84cf1f5d21a106b6de5c0
benaja-bendo <benaja.bendo02@gmail.com>
2022-08-29 00:04:39 +0200

    add migration

---
+++ database/migrations/2022_08_28_215548_create_user_infos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('user_infos', function (Blueprint $table) {
+            $table->id();
+            $table->string('first_name')->nullable();
+            $table->string('last_name')->nullable();
+            $table->string('address')->nullable();
+            $table->string('code_postal')->nullable();
+            $table->enum('genre',['M','F']);
+            $table->string('city')->nullable();
+            $table->string('phone')->nullable();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('user_infos');
+    }
+};
