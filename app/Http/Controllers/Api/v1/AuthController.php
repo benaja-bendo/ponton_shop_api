1967731d181876e83d2c04e2e538d1c4f741be0e
benaja-bendo <benaja.bendo02@gmail.com>
2022-09-01 13:42:58 +0200

    login and register

--- app/Http/Controllers/Api/v1/AuthController.php
+++ app/Http/Controllers/Api/v1/AuthController.php
@@ -8,80 +8,105 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Password;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 
 class AuthController extends Controller
 {
+    /**
+     * Create User
+     * @param Request $request
+     * @return User
+     */
     public function createUser(Request $request)
     {
-        $validatedData = $request->validate([
-            'name' => 'required|string|max:255',
-            'email' => 'required|string|email|max:255|unique:users',
-            'password' => 'required|string|min:8|confirmed',
-        ]);
-        $user = User::create([
-            'name' => $validatedData['name'],
-            'email' => $validatedData['email'],
-            'password' => Hash::make($validatedData['password']),
-        ]);
-        $token = $user->createToken('auth_token')->plainTextToken;
+        try {
+            $validateUser = Validator::make($request->all(), [
+                "name" => "required",
+                "email" => "required|email|unique:users,email",
+                "password" => "required"
+            ]);
 
-        return response()->json([
-            "success" => true,
-            "message" => "user created successfully",
-            "data" => [
-                "user" => $user,
-                'access_token' => $token,
-                'token_type' => 'Bearer',
-            ]
-        ], 201);
+            if ($validateUser->fails()) {
+                return response()->json([
+                    "success" => false,
+                    "message" => "validation error",
+                    "errors" => $validateUser->errors()
+                ], 401);
+            }
+            $user = User::create([
+                "name" => $request->name,
+                "email" => $request->email,
+                "password" => Hash::make($request->password)
+            ]);
+            return response()->json([
+                "success" => true,
+                "message" => "user created successfully",
+                "data" => [
+                    "user" => $user,
+                    'access_token' => $user->createToken("create_user")->plainTextToken,
+                    'token_type' => 'Bearer',
+                ]
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "status" => false,
+                "message" => $th->getMessage(),
+            ], 500);
+        }
     }
 
+    /**
+     * login User
+     * @param Request $request
+     * @return User
+     */
     public function loginUser(Request $request)
     {
-        $validatedData = $request->validate([
-            'email' => 'required|string|email|max:255|exists:users,email',
-            'password' => 'required|string|min:8|confirmed',
-        ]);
-        if (!Auth::attempt($request->only('email', 'password'))) {
+        try {
+            $validatUser = Validator::make($request->all(), [
+                "email" => "required|email",
+                "password" => "required"
+            ]);
+
+            if ($validatUser->fails()) {
+                return response()->json([
+                    "success" => false,
+                    "message" => "validator error",
+                    "errors" => $validatUser->errors()
+                ], 401);
+            }
+            if (!Auth::attempt($request->only(["email", "password"]))) {
+                return response()->json([
+                    "success" => false,
+                    "message" => "identifiants non valide"
+                ], 401);
+            }
+            $user = User::where('email', $request->email)->first();
             return response()->json([
-                'message' => 'Invalid login details'
-            ], 401);
+                "success" => true,
+                "message" => "user Logged In Successefully",
+                "data" => [
+                    "user" => $user,
+                    'access_token' => $user->createToken("create_user")->plainTextToken,
+                    'token_type' => 'Bearer',
+                ]
+            ], 201);
+        } catch (\Throwable $th) {
+            return response()->json([
+                "status" => false,
+                "message" => $th->getMessage(),
+            ], 500);
         }
-
-        $user = User::where('email', $request['email'])->firstOrFail();
-
-        $token = $user->createToken('auth_token')->plainTextToken;
-
-        return response()->json([
-            "success" => true,
-            "message" => "login successfully",
-            "data" => [
-                "user" => $user,
-                'access_token' => $token,
-                'token_type' => 'Bearer',
-            ]
-        ], 200);
     }
 
-    public function forgotPassword(Request $request)
+    public function logout(Request $request)
     {
-        $validation = $request->validate([
-            'email' => 'required|email|max:255',
-        ]);
-        $status = Password::sendResetLink(
-            $request->only('email')
-        );
-        // if ($status == Password::RESET_LINK_SENT) {
-        //     return [
-        //         'status' => __($status)
-        //     ];
-        // }
-        // throw ValidationException::withMessages([
-        //     'email' => [trans($status)],
-        // ]);
-        return $status === Password::RESET_LINK_SENT
-            ? back()->with(['status' => __($status)])
-            : back()->withErrors(['email' => __($status)]);
+        auth()->user()->tokens()->delete();
+
+        return [
+            "status" => true,
+            "message" => "Logged out",
+        ];
     }
 }
